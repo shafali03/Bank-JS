8ed9208d091b1c9dce6845b46a35e93511f88a9d
shafali03 <shaf03sm@gmail.com>
2020-01-27 20:28:19 +0000

    first test current balance is $2

---
+++ spec/AccountSpec.js
@@ -0,0 +1,12 @@
+'use strict'
+
+describe('Account', function() {
+
+  var account;
+
+  it('the current account balance is $2', function() {
+    account = new Account();
+    expect(account.currentBalance()).toEqual(2)
+  });
+  
+});
