54b7adf4c7e8270271dc3d5fcab7b0140d214063
shafali03 <shaf03sm@gmail.com>
2020-01-28 22:04:02 +0000

    can withdraw from current balance

--- spec/AccountSpec.js
+++ spec/AccountSpec.js
@@ -4,15 +4,21 @@ describe('Account', function() {
 
   var account;
 
-  it('current account balance is $0', function(){
+  it('current account balance is $10', function(){
     account = new Account();
-    expect(account.currentBalance()).toEqual(0)
+    expect(account.currentBalance()).toEqual(10)
   });
 
-  it('has a deposit of $10 has been added to the account', function(){
+  it('has a deposit of $5 added to the account', function(){
     account = new Account();
     account.deposit()
-    expect(account.currentBalance()).toEqual(10)
+    expect(account.currentBalance()).toEqual(15)
+  })
+
+  it('can withdraw $5 from account', function() {
+    account = new Account();
+    account.withdraw()
+    expect(account.currentBalance()).toEqual(5)
   })
 
 });
--- src/Account.js
+++ src/Account.js
@@ -1,9 +1,19 @@
 'use strict'
 
 function Account() {
-  this.balance = 0
+  this.balance = 10
+  this.balance += 5
+  this.balance -= 5
 }
 
 Account.prototype.currentBalance = function(){
   return this.balance
+}
+
+Account.prototype.deposit = function(){
+  this.balance += 5
+}
+
+Account.prototype.withdraw = function() {
+  this.balance -= 5
 }
