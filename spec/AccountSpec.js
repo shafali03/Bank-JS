e5d30d9c6c9094c75b103fec5b224629e452edd7
shafali03 <shaf03sm@gmail.com>
2020-01-28 22:09:38 +0000

    added beforeEach for account

--- spec/AccountSpec.js
+++ spec/AccountSpec.js
@@ -4,19 +4,21 @@ describe('Account', function() {
 
   var account;
 
-  it('current account balance is $10', function(){
+  beforeEach(function() {
     account = new Account();
+  })
+
+
+  it('current account balance is $10', function(){
     expect(account.currentBalance()).toEqual(10)
   });
 
   it('has a deposit of $5 added to the account', function(){
-    account = new Account();
     account.deposit()
     expect(account.currentBalance()).toEqual(15)
   })
 
   it('can withdraw $5 from account', function() {
-    account = new Account();
     account.withdraw()
     expect(account.currentBalance()).toEqual(5)
   })
