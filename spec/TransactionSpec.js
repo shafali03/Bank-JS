cabeb4065aa5e60b7e461ea366d675ee74a8356f
shafali03 <shaf03sm@gmail.com>
2020-01-30 21:37:54 +0000

    fixed error with transaction credit

--- spec/TransactionSpec.js
+++ spec/TransactionSpec.js
@@ -5,10 +5,10 @@ describe('Transaction', function() {
   var transaction
 
   beforeEach(function() {
-    transaction = new Transaction('credit', 10);
+    transaction = new Transaction('credit', 20, 10);
   })
 
   it('has credit of $20', function() {
-    expect.transaction.credit().toEqual(10)
+    expect(transaction.credit).toEqual(20)
   })
 })
